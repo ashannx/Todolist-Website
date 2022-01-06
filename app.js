aeb809414d13924ab38972b62d53fcb00035802c
Ashan <ashannxx@gmail.com>
2022-01-06 16:49:02 -0700

    prep for heroku

--- app.js
+++ app.js
@@ -336,14 +336,12 @@ async function main() {
 
 } // end of main
 
-
-
-
-
-
-
 main().catch((err) => console.log(err));
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started on port " + port);
 });
